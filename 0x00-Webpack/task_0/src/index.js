1474a53859be06e0ad9d1ea3e5d171807a560829
ysamwel <yvonneatieno2000@gmail.com>
2024-08-19 20:27:21 +0300

    Task 0

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,6 +1,5 @@
 import $ from "jquery";
-$(function() {
+
     $('body').append('<p>This is the first paragraph.</p>');
     $('body').append('<p>This is the second paragraph.</p>');
     $('body').append('<p>This is the third paragraph.</p>');
-});
