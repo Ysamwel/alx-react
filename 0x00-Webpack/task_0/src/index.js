db22221130c8659794afe07facf855cabd96e807
ysamwel <yvonneatieno2000@gmail.com>
2024-08-19 20:15:37 +0300

    Task 0

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,6 @@
+import $ from "jquery";
+$(function() {
+    $('body').append('<p>This is the first paragraph.</p>');
+    $('body').append('<p>This is the second paragraph.</p>');
+    $('body').append('<p>This is the third paragraph.</p>');
+});
