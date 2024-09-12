6f9e0a4560159550930c3c84f255141d0b41c0af
BarakaCaleb <calebbaraka79@gmail.com>
2024-09-12 23:48:40 +0300

    Task 6 changes

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,6 +1,5 @@
 import { Map } from 'immutable';
 
-// Correct export default syntax for named function
 export default function mergeDeeplyElements(page1, page2) {
-    return Map(page1).mergeDeep(Map(page2));
+  return Map(page1).mergeDeep(Map(page2));
 }
