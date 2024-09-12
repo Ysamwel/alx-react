6bc2998ce6c662e131b2445736b211a3d652438e
BarakaCaleb <calebbaraka79@gmail.com>
2024-09-12 23:12:50 +0300

    Task one debugging

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,5 @@
 import { fromJS } from "./node_modules/immutable/dist/immutable";
 
-export default getImmutableObject(object){
+export default getImmutableObject (object) {
   return fromJS(object);
 }
