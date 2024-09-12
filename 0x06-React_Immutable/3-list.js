ef44b7e64dbcbdf43703e02d079d4cbb5c6158d1
BarakaCaleb <calebbaraka79@gmail.com>
2024-09-12 22:00:27 +0300

    Task 4

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,11 +1,12 @@
+import { List } from 'immutable';
 
-export default function getListObject(array) {
+export function getListObject(array) {
     const immutableList = fromJS(array);
 
     return immutableList;
 }
 
-export default function addElementToList(list, element) {
+export function addElementToList(list, element) {
     list.push(element);
 
     return list;
