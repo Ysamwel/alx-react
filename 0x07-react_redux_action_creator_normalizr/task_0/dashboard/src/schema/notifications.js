3a9e3545ca719a5c254a86a917efe00a31e208a5
BarakaCaleb <calebbaraka79@gmail.com>
2024-09-20 03:37:20 +0300

    changes

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++
@@ -1,5 +0,0 @@
-import * as notificationsData from '../../../../notifications.json';
-export default function getAllNotificationsByUser(userId){
-    return notificationsData.filter((notification) => notification.author.id === userId).map((notification) => notification.context);
-}
-
