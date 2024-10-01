774d4c9f9845b845cb920de8701082186edf78f4
ysamwel <yvonneatieno2000@gmail.com>
2024-10-01 20:23:12 +0300

    store

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import uiReducer from './reducers/uiReducer';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const store = createStore(uiReducer);
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
