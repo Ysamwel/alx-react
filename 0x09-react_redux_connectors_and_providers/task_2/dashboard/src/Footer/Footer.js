e9ef73f058056793f04e04a7ff1ae2fa1f8b352a
ysamwel <yvonneatieno2000@gmail.com>
2024-10-02 17:04:51 +0300

    connecting-user-state-footer

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { getFullYear, getFooterCopy } from '../utils/utils';
-import { AppContext } from '../App/AppContext';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-function Footer() {
+function Footer({ user }) {
   return (
-    <AppContext.Consumer>
-      {(context) => {
-        return (
-          <footer>
-            <p>
-              Copyright {getFullYear()} - {getFooterCopy(true)}
-            </p>
-            {context.user.isLoggedIn && <a href='#'>Contact us</a>}
-          </footer>
-        );
-      }}
-    </AppContext.Consumer>
+    <footer>
+      <p>
+        Copyright {getFullYear()} - {getFooterCopy(true)}
+      </p>
+      {user.isLoggedIn && <a href='#'>Contact us</a>}
+    </footer>
   );
 }
 
-export default Footer;
+// Define propTypes for user prop
+Footer.propTypes = {
+  user: PropTypes.shape({
+    isLoggedIn: PropTypes.bool,
+  }),
+};
+
+// Define defaultProps in case user is not passed
+Footer.defaultProps = {
+  user: { isLoggedIn: false },
+};
+
+// mapStateToProps to retrieve the user from Redux state
+const mapStateToProps = (state) => ({
+  user: state.user, // Adjust according to your state shape
+});
+
+// Connecting the Footer component to Redux
+export default connect(mapStateToProps)(Footer);
