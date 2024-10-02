3285d4850afaa467d97f4aeb93b6ec26b838225d
ysamwel <yvonneatieno2000@gmail.com>
2024-10-02 16:14:17 +0300

    update index.js

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import uiReducer from './reducers/uiReducer';
 import { createRoot } from 'react-dom/client';
 
-const store = createStore(uiReducer);
+const store = createStore(uiReducer, applyMiddleware(thunk));
 const container = document.getElementById('root');
 const root = createRoot(container);
 
