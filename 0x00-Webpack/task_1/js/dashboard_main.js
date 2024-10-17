3c212c1bf469c658a150e307ffe9811c7608044a
Ysamwel <yvonneatieno2000@gmail.com>
2024-10-17 23:42:08 +0300

    react

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -3,20 +3,15 @@ import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-
-$('body').append('<button id="clicker">Click here to get started</button>');
-
+$('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
-
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
+let conunt = 0;
 
 function updateCounter() {
-    count++;
-    $('#count').text(`${count} clicks on the button`);
-}
+  count++;
+  $("#count").html(`${count} clicks on the button`);
+};
 
-const debouncedUpdateCounter = _.debounce(updateCounter, 500);
-
-$('#clicker').on('click', debouncedUpdateCounter);
+$('button').on('click', _.debounce(updateCounter, 500));
